df4b638bb9956752f7bf724da71652cc76c069bc
Luca Botini <devbotini@gmail.com>
2023-11-12 21:20:40 -0300

    att bank

--- Banco DoWhile/index.js
+++ Banco DoWhile/index.js
@@ -1,29 +1,44 @@
+let soun = false
+const nome = prompt('Qual seu nome?')
+const cpf = parseInt(prompt('Qual seu CPF? (xxxxxxxxxxx)'))
 
+let saldoTotal = 10
+let maiorValor = 0
 do {
-    const nome = prompt('Qual seu nome?')
-    const cpf = parseInt(prompt('Qual seu CPF? (xxxxxxxxxxx)'))
-
     const saqdep = parseInt(prompt('Depósito(1) Saque(2)'))
-    const valorInserido = parseInt(prompt('Qual valor inserido?'))
-    let saldoTotal = 10
+    const valorInserido = parseInt(prompt('Qual valor inserido? Seu saldo é de: ' + saldoTotal + ' reais'))
+
     switch (saqdep) {
         case 1:
-            if (saqdep == 1) {
+            if (saqdep == 1 && valorInserido > 0 ){
                 saldoTotal = saldoTotal + valorInserido
-
-            }
+                }else {
+                    alert('Você não pode inserir valores abaixo ou igual a (0)')
+                }
             break;
         case 2:
-            if (saqdep == 2) {
-                saldoTotal = saldoTotal - valorInserido
-
-            }
+            if (valorInserido < saldoTotal) {
+                    saldoTotal = saldoTotal - valorInserido
+                }else{
+                    alert('Você não pode inserir valores abaixo ou igual a (0)')
+                }
             break;
 
         default:
+            alert('Insira uma opção válida')
             break;
     }
-let soun = 'n'
 
-soun = parseInt(prompt('Deseja encerrar o processo? (S/N)'))
-} while (soun == 'n')
+
+    if (valorInserido > maiorValor) {
+        maiorValor = valorInserido
+    }
+
+    let soun2 = prompt('Deseja encerrar o processo? (S/N)')
+    if (soun2 == 's') {
+        soun = true
+    }
+} while (soun == false)
+
+alert('Seu saldo é de: ' + saldoTotal + ' reais')
+alert('O maior valor inserido foi: ' + maiorValor + ' reais')
